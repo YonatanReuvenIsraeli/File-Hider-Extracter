ccd0a833d79c4d2404a6a0b6952b00ac9d176979
Yonatan Israeli <yonirisraeli@gmail.com>
2024-03-19 20:38:14 -0400

    Update File Hider-Extracter.bat

--- File Hider-Extracter.bat
+++ File Hider-Extracter.bat
@@ -9,58 +9,58 @@ echo [1] Hide File
 echo [2] Extract File
 echo [3] Close
 set /p input="What do you want to do? (1/3) "
-if /i "%input%"=="1" goto :1
-if /i "%input%"=="2" goto :2
-if /i "%input%"=="2" goto :4
+if /i "%input%"=="1" goto 1
+if /i "%input%"=="2" goto 2
+if /i "%input%"=="2" goto 4
 
 echo Invalid Syntax
-goto :Main
+goto Main
 
 :1
 echo.
 set /p DriveLetter="What drive are the files in? (A:-Z:)
-if /i "%DriveLetter%"=="A:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="B:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="C:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="D:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="E:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="F:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="G:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="H:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="I:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="J:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="K:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="L:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="M:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="N:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="O:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="P:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Q:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="R:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="S:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="T:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="U:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="V:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="W:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="X:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Y:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Z:" goto :SureDriveLetter
+if /i "%DriveLetter%"=="A:" goto SureDriveLetter
+if /i "%DriveLetter%"=="B:" goto SureDriveLetter
+if /i "%DriveLetter%"=="C:" goto SureDriveLetter
+if /i "%DriveLetter%"=="D:" goto SureDriveLetter
+if /i "%DriveLetter%"=="E:" goto SureDriveLetter
+if /i "%DriveLetter%"=="F:" goto SureDriveLetter
+if /i "%DriveLetter%"=="G:" goto SureDriveLetter
+if /i "%DriveLetter%"=="H:" goto SureDriveLetter
+if /i "%DriveLetter%"=="I:" goto SureDriveLetter
+if /i "%DriveLetter%"=="J:" goto SureDriveLetter
+if /i "%DriveLetter%"=="K:" goto SureDriveLetter
+if /i "%DriveLetter%"=="L:" goto SureDriveLetter
+if /i "%DriveLetter%"=="M:" goto SureDriveLetter
+if /i "%DriveLetter%"=="N:" goto SureDriveLetter
+if /i "%DriveLetter%"=="O:" goto SureDriveLetter
+if /i "%DriveLetter%"=="P:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Q:" goto SureDriveLetter
+if /i "%DriveLetter%"=="R:" goto SureDriveLetter
+if /i "%DriveLetter%"=="S:" goto SureDriveLetter
+if /i "%DriveLetter%"=="T:" goto SureDriveLetter
+if /i "%DriveLetter%"=="U:" goto SureDriveLetter
+if /i "%DriveLetter%"=="V:" goto SureDriveLetter
+if /i "%DriveLetter%"=="W:" goto SureDriveLetter
+if /i "%DriveLetter%"=="X:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Y:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Z:" goto SureDriveLetter
 echo Invalid Syntax!
-goto :CheckExist
+goto CheckExist
 
 :CheckExist
-if not exist "%DriveLetter%" goto :NotExist1
+if not exist "%DriveLetter%" goto NotExist1
 %DriveLetter%
-goto :Path1
+goto Path1
 
 :NotExist1
 echo %DriveLetter% does not exist. Please try again.
-goto :1
+goto 1
 
 :Path1
 echo.
 set /p Path="What is the full path to your folder that contains the files?
-if not exist "%Path%" goto :NotExistPath1
+if not exist "%Path%" goto NotExistPath1
 cd %Path%
 echo.
 set /p Hide1="Which file are you trying to hide? "
@@ -72,57 +72,57 @@ type  %Hide1% > %Hide2%:%Hide3%
 echo.
 echo Alternate Data Stream created! Press any key to go to the main menu.
 pause >nul
-goto :Main
+goto Main
 
 :NotExistPath1
 echo %Path% does not exist. Please try again.
-goto :1
+goto 1
 
 :2
 echo.
 set /p DriveLetter="What drive are the file in? (A:-Z:) "
-if /i "%DriveLetter%"=="A:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="B:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="C:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="D:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="E:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="F:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="G:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="H:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="I:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="J:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="K:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="L:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="M:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="N:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="O:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="P:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Q:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="R:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="S:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="T:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="U:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="V:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="W:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="X:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Y:" goto :SureDriveLetter
-if /i "%DriveLetter%"=="Z:" goto :SureDriveLetter
+if /i "%DriveLetter%"=="A:" goto SureDriveLetter
+if /i "%DriveLetter%"=="B:" goto SureDriveLetter
+if /i "%DriveLetter%"=="C:" goto SureDriveLetter
+if /i "%DriveLetter%"=="D:" goto SureDriveLetter
+if /i "%DriveLetter%"=="E:" goto SureDriveLetter
+if /i "%DriveLetter%"=="F:" goto SureDriveLetter
+if /i "%DriveLetter%"=="G:" goto SureDriveLetter
+if /i "%DriveLetter%"=="H:" goto SureDriveLetter
+if /i "%DriveLetter%"=="I:" goto SureDriveLetter
+if /i "%DriveLetter%"=="J:" goto SureDriveLetter
+if /i "%DriveLetter%"=="K:" goto SureDriveLetter
+if /i "%DriveLetter%"=="L:" goto SureDriveLetter
+if /i "%DriveLetter%"=="M:" goto SureDriveLetter
+if /i "%DriveLetter%"=="N:" goto SureDriveLetter
+if /i "%DriveLetter%"=="O:" goto SureDriveLetter
+if /i "%DriveLetter%"=="P:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Q:" goto SureDriveLetter
+if /i "%DriveLetter%"=="R:" goto SureDriveLetter
+if /i "%DriveLetter%"=="S:" goto SureDriveLetter
+if /i "%DriveLetter%"=="T:" goto SureDriveLetter
+if /i "%DriveLetter%"=="U:" goto SureDriveLetter
+if /i "%DriveLetter%"=="V:" goto SureDriveLetter
+if /i "%DriveLetter%"=="W:" goto SureDriveLetter
+if /i "%DriveLetter%"=="X:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Y:" goto SureDriveLetter
+if /i "%DriveLetter%"=="Z:" goto SureDriveLetter
 echo Invalid Syntax!
-goto :CheckExist
+goto CheckExist
 
 :CheckExist
-if not exist "%DriveLetter%" goto :NotExist2
+if not exist "%DriveLetter%" goto NotExist2
 %DriveLetter%
-goto :Path2
+goto Path2
 
 :NotExist2
 echo %DriveLetter% does not exist. Please try again.
-goto :2
+goto 2
 
 :Path2
 echo.
 set /p Path="What is the full path to your folder that contains the files? "
-if not exist "%Path%" goto :NotExistPath2
+if not exist "%Path%" goto NotExistPath2
 cd %Path%
 echo.
 set /p Show1="Which file that the Alternate Data Stream is hidden in? "
@@ -134,11 +134,11 @@ expand %Show1%:%Show2% %Show3%
 echo.
 echo Alternate Data Stream created! Press any key to go to the main menu.
 pause >nul
-goto :Main
+goto Main
 
 :NotExistPath2
 echo %Path% does not exist. Please try again.
-goto :2
+goto 2
 
 :Main
 echo.
@@ -147,11 +147,11 @@ echo [1] Hide File
 echo [2] Extract File
 echo [3] Close
 set /p input="What do you want to do? (1/3) "
-if /i "%input%"=="1" goto :1
-if /i "%input%"=="2" goto :2
-if /i "%input%"=="2" goto :
+if /i "%input%"=="1" goto 1
+if /i "%input%"=="2" goto 2
+if /i "%input%"=="3" goto 3
 echo Invalid Syntax
-goto :Main
+goto Main
 
 :3
 endlocal
