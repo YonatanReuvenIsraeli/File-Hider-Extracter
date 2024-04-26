e1f4f44e7ec326047a38794d537ad2003789280e
Yonatan Israeli <yonirisraeli@gmail.com>
2024-04-25 23:06:54 -0400

    Update File Hider-Extracter.bat

--- File Hider-Extracter.bat
+++ File Hider-Extracter.bat
@@ -2,7 +2,7 @@
 setlocal
 title File Hider/Extracter
 echo Program Name: File Hider/Extracter
-echo Version: 1.2.1
+echo Version: 1.3.0
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -35,6 +35,14 @@ set Hide3=
 set /p Hide3="What will you like to name this alternate data stream? "
 goto Overwrite
 
+:NotHide1
+echo "%Hide1%" does not exist!
+goto 1
+
+:NotHide2
+echo "%Hide2%" does not exist!
+goto 1
+
 :Overwrite
 set overwrite=
 set /p overwrite="This will overwrite an alternate data stream with the same name at "%Hide1%" if it exists. Are you sure you want to continue? (Yes/No)
@@ -44,18 +52,15 @@ echo Invalid Syntax!
 goto Overwrite
 
 :Hide
-type "%Hide1%" > "%Hide2%":"%Hide3%"
+type "%Hide1%" > "%Hide2%":"%Hide3%" > nul 2>&1
+if not "%errorlevel%"=="0" goto HideError
 echo.
 echo Alternate data stream created! Press any key to go to the main menu.
 pause > nul 2>&1
 goto Start
 
-:NotHide1
-echo "%Hide1%" does not exist!
-goto 1
-
-:NotHide2
-echo "%Hide2%" does not exist!
+:HideError
+echo There has been an error you can try again.
 goto 1
 
 :2
@@ -69,35 +74,33 @@ set /p Show2="What is the name of the alternate data stream? "
 echo.
 set Show3=
 set /p Show3="What do you want the full path to the folder of the extracted file to be? "
-if not exist "%Show3%" goto Error1
+if not exist "%Show3%" goto ShowError1
 echo.
 set Show4=
 set /p Show4="What to you want the extracted file do be named? "
-if exist "%Show3%\%Show4%" goto Error2
-expand "%Show1%":"%Show2%" "%Show3%\%Show4%"
-if exist "%Show3%\%Show4%" goto Success
-echo Error! Invalid file name. Please try again.
-goto 2
+if exist "%Show3%\%Show4%" goto ShowError2
+expand "%Show1%":"%Show2%" "%Show3%\%Show4%" > nul 2>&1
+if not "%errorlevel%"=="0" goto ShowError3
+echo.
+echo Alternate data stream exracted! Your extracted file is at "%Show3%\%Show4%". Press any key to go to the main menu.
+goto Start
 
 :NotShow1
 echo "%Show1%" does not exist!
 goto 2
 
-:Error1
-echo.
+:ShowError1
 echo Error! Path not found. Please try again.
 goto 2
 
-:Error2
-echo.
+:ShowError2
 echo Error! File already exists. Please rename "%Show3%\%Show4%" or move "%Show3%\%Show4%" to another location and try again.
 goto 2
 
-:Success
-echo.
-echo Alternate data stream exracted! Your extracted file is at "%Show3%\%Show4%". Press any key to go to the main menu.
-pause > nul 2>&1
-goto Start
+
+:ShowError3
+echo Error! Invalid file name. Please try again.
+goto 2
 
 :3
 endlocal
